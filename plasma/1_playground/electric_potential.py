5c580065aabb4f6d94ce23be4df70dceab53cd2c
Ayumu-Nono <ayumu.bete@gmail.com>
2020-07-02 07:53:48 +0900

    add calculation

--- plasma/1_playground/electric_potential.py
+++ plasma/1_playground/electric_potential.py
@@ -1,9 +1,20 @@
 import numpy as np
+import math
 
 
 class ElectricPotentialModel:
     def __init__:
-        pass
+        self.Boltzmann_constant = 1.380649E-23
+        self.electron_temparature = 1.16E4  # 適当
+
+    def calc_electric_potential_without_magnetic_field(
+        self,
+        ion_density: float
+    ) -> float:
+        k = self.Boltzmann_constant
+        T = self.electron_temparature
+        potential = k * T * math.log(ion_density)
+        return potential
 
 def main():
     potential = ElectricPotentialModel()
