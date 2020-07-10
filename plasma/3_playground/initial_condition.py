52ad031bb479863323d7925781846828a0eee3c1
Ayumu-Nono <ayumu.bete@gmail.com>
2020-07-10 09:20:18 +0900

    add init class

---
+++ plasma/3_playground/initial_condition.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class InitialCondition:
+    def make_latice(
+        self,
+        dimention,
+        num,
+        scale,
+        ):
+        pass
+
+    def make_engine(
+        self,
+        radius,
+        position,
+        ):
+        pass
+
+    def test(self):
+        print()
+
+
+def main():
+    init = InitialCondition()
+
+
+if __name__ == "__main__":
+    main()
