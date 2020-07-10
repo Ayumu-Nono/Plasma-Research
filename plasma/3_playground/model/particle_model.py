8cf147aa95d7c46ce44a6d498436151e365fb06a
Ayumu-Nono <ayumu.bete@gmail.com>
2020-07-10 14:33:23 +0900

    add property in model

--- plasma/3_playground/model/particle_model.py
+++ plasma/3_playground/model/particle_model.py
@@ -2,6 +2,11 @@ import numpy as np
 
 
 class ParticleModel:
+    # default
+    self.type: str = "neutral"
+    self.charge: float = 0
+    self.mass: float = 131 / 6.022E23
+
     def __init__(
         self,
         pk: int,
@@ -10,6 +15,17 @@ class ParticleModel:
     ) -> None:
         self.position = init_position
         self.velocity = init_velocity
+
+    def as_neutral(self) -> None:
+        pass
+
+    def as_ion(self) -> None:
+        self.type = "ion"
+        self.charge = 1.602E-19
+
+    # def as_electron(self) -> None:
+    #     self.type = "electron"
+    #     self.charge = 1.602E-19
 
     def change_status(
         self,
