c5970448c21a3c67d503c973454e8c50e57288de
Ayumu-Nono <ayumu.bete@gmail.com>
2020-07-10 13:42:29 +0900

    add particle model

---
+++ plasma/3_playground/initialize/init_neutral_particle.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+class InitializeNeutralPatricleModel:
+    def __init__(
+        self,
+        num: int,
+    ):
+        self.particle_num = num
+
+    def 
+
+def main():
+    init = InitializeNeutralPatricleModel(1000)
+
+
+if __name__ == "__main__":
+    main()
