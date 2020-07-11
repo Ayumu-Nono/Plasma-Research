0f2625bb020add2a9dd7a0af603cbba3847421e3
Ayumu-Nono <ayumu.bete@gmail.com>
2020-07-11 20:31:25 +0900

    add para with physical quantity

--- plasma/3_playground/calc/electric_potential.py
+++ plasma/3_playground/calc/electric_potential.py
@@ -1,21 +1,21 @@
 import numpy as np
 import math
 
+from ..quantity.initial_quantity import PhysicalQuantity
 
 class ElectricPotentialModel:
 
     """電位を定義"""
     
-    def __init__:
-        self.Boltzmann_constant = 1.380649E-23  # [J K^-1]
-        self.electron_temparature = 1.16E4 * 1  # [K] 数値は適当
-
+    def __init__(self):
+        self.physical_quantity = PhysicalQuantity()
+        
     def calc_electric_potential_with_no_collision(
         self,
         ion_density: float
     ) -> float:
-        k = self.Boltzmann_constant
-        T = self.electron_temparature
+        k = self.physical_quantity.Boltzmann_constant
+        T = self.physical_quantity.electron_temparature
         potential = k * T * math.log(ion_density)
         return potential
 
