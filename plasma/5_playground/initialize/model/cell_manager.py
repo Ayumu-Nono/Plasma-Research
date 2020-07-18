17c3a0075b5a766021c6801f82e4718f56edc001
Ayumu-Nono <ayumu.bete@gmail.com>
2020-07-18 13:12:52 +0900

    add CellsManager

--- plasma/5_playground/initialize/model/cell_manager.py
+++ plasma/5_playground/initialize/model/cell_manager.py
@@ -5,26 +5,22 @@ class CellManager:
 
     """"Cellをコントロールするクラス"""
 
-    def __init__(self, cells: np.array) -> None:
-        self.cells = cells
-
-    def init_cells(self) -> None:
-        for i in range(10):
-            for j in range(10):
-                for k in range(10):
-                    self.cells[i, j, k] = 
+    def __init__(self, cells_num: int) -> None:
+        self.cells = [[[[] for i3 in range(cells_num)] for i2 in range(cells_num)] for i1 in range(cells_num)]
 
     def save_particles_to_cell(
         self,
         particles_list: list,
     ) -> None:
-        cells_num = len(self.cells)
-        i = 0
-        j = 0
-        k = 0
-        # self.cells = 
+        for particle in particles_list:
+            position = particle.position
+            position_floor = np.floor(position)
+            cell_x = position_floor[0]
+            cell_y = position_floor[1]
+            cell_z = position_floor[2]
+            self.cells[cell_x][cell_y][cell_z].append(particle)    
 
 
 if __name__ == "__main__":
-    cell_manager = CellManager(np.zeros((10, 10, 10)))
+    cell_manager = CellManager(cells_num=3)
     cell_manager.init_cells()
