84d95fb2b3a9ee274dbbe919b75acadb33be33e9
Ayumu-Nono <ayumu.bete@gmail.com>
2020-08-10 10:10:27 +0900

    ave velocity

--- plasma/6_playground/calc/charge_exchange.py
+++ plasma/6_playground/calc/charge_exchange.py
@@ -7,35 +7,46 @@ root_dir = pathlib.Path(__file__).resolve().parent.parent
 sys.path.append(str(root_dir))
 
 from utils.random_module import RandomModule
+from utils.pic_module import PICModule
 from models import numerical_quantity as nq
 from models import physical_quantity as pq
 from models import Particle
 
 
-class ChargeExchange:
-    def __init__(self):
-        self.random_module = RandomModule()
+class ChargeExchange(PICModule):
     
-    def is_collide(
+    def __init__(
         self,
-        ion_particle: Particle,
-        neutral_particle: Particle,
-    ) -> bool:
-        distance = np.linalg.norm(
-            ion_particle.position - neutral_particle.position
-        )
-        collision_distance = nq.COLLOSION_DISTANCE
-        return distance < collision_distance
-
-    def exchange_velocity(
-        self,
-        ion_particle: Particle,
-        neutral_particle: Particle,
+        density_grid: np.array,
+        velocity_x: np.array,
+        velocity_y: np.array,
+        velocity_z: np.array
     ) -> None:
-        ion_velocity = ion_particle.velocity
-        neutral_velocity = neutral_particle.velocity
-        ion_particle.velocity = neutral_velocity
-        neutral_particle.velocity = ion_velocity
+        self.random_module = RandomModule()
+        self.density_grid = density_grid
+        self.velocity_list = [velocity_x, velocity_y, velocity_z]
+
+    def calc_ave_velocity_on_all_grid(
+        self,
+        velocity_grid_in_sum: np.array,
+        density_grid: np.array
+    ) -> np.array:
+        velocity_grid_in_ave = np.divide(
+            velocity_grid_in_sum,
+            density_grid,
+            out=np.zeros_like(density_grid),
+            where=density_grid!=0)
+        return velocity_grid_in_ave
+
+    def average_velocity_list(self) -> None:
+        new_velocity_list = []
+        for velocity_array in self.velocity_list:
+            new_velocity_array = self.calc_ave_velocity_on_all_grid(
+                velocity_grid_in_sum=velocity_array,
+                density_grid=self.density_grid
+            )
+            new_velocity_list.append(new_velocity_array)
+        self.velocity_list = new_velocity_list
 
     def generate_rate(
         self,
