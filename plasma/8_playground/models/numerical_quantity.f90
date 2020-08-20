8b423d4adbafb281aad1dff4eab0a330cfeb5740
Ayumu-Nono <ayumu.bete@gmail.com>
2020-08-21 07:42:57 +0900

    add initial particle num para

--- plasma/8_playground/models/numerical_quantity.f90
+++ plasma/8_playground/models/numerical_quantity.f90
@@ -1,8 +1,9 @@
 module numerical_quantity
     implicit none
-    real, parameter :: STEP_SIZE = 0.001
+    integer, parameter :: LATTICE_NUM = 100  ! [個]
     integer, parameter :: END_STEP = 10000
+    integer, parameter :: INITIAL_PARTICLE_NUM = 1d3  ! [個]
     real, parameter :: ENGINE_RADIUS = 3
     real, parameter :: ENGINE_LENGTH = 3 ! [m]
-    integer, parameter :: LATTICE_NUM = 100  ! [個]
+    real, parameter :: STEP_SIZE = 0.001
 end module numerical_quantity
