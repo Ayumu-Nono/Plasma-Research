73751e9dbcf3e5df5443ef36eb556e6cae446fc9
Ayumu-Nono <ayumu.bete@gmail.com>
2020-08-22 20:44:33 +0900

    make_particle_array

--- plasma/8_playground/models/particle.f90
+++ plasma/8_playground/models/particle.f90
@@ -1,10 +1,21 @@
 module particle
     implicit none
 contains
-    subroutine make_particle_array(particle_num)
-        integer particle_num
-        real :: particle_array(5) = 0
-        print *, particle_num
-        print *, particle_array(:)
-    end subroutine make_particle_array
+    function make_particle_array(particle_num)
+        implicit none
+        integer, intent(in) :: particle_num
+        real :: make_particle_array(particle_num)
+        make_particle_array(:) = 0
+    end function
+
+    ! subroutine make_particle_array(particle_num)
+    !     implicit none
+    !     integer, intent(in) :: particle_num
+    !     real, intent(out) :: return_array
+    !     real :: particle_array(particle_num)
+    !     print *, particle_num
+    !     print *, particle_array(:)
+    ! end subroutine make_particle_array
 end module particle
+
+! TODO: 返り値に配列
